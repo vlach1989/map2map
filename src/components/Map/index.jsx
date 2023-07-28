33da6608936f477eec9b116f15ba5818a383f805
PavelVlach <vlach1989@gmail.com>
2023-07-28 13:18:42 +0200

    map sync

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,29 +1,94 @@
-import {MapContainer, WMSTileLayer} from 'react-leaflet';
+import {useCallback, useEffect, useMemo, useState} from 'react';
+import {MapContainer} from 'react-leaflet';
 import PropTypes from 'prop-types';
 import './style.css';
 
-function Map({crs, center, zoom}) {
-	return (
-		<MapContainer center={center} zoom={zoom} className="m2m-Map" crs={crs}>
-			<WMSTileLayer
-				url={
-					// 'https://ags.cuzk.cz/arcgis1/services/ORTOFOTO/MapServer/WMSServer',
-					'https://gis.cenia.cz/mapcache/ortofotomapa_historicka/wms?'
-				}
-				params={{
-					layers: 'orto',
-					// layers: '0',
-				}}
+function Map({
+	crs,
+	center,
+	zoom,
+	zoomControl = true,
+	attributionControl = true,
+	onCenterChange,
+	onZoomChange,
+	children,
+}) {
+	const [map, setMap] = useState(null);
+	const [lat, lon] = center;
+
+	useEffect(() => {
+		if (map) {
+			const currentZoom = map.getZoom();
+			const currentCenter = map.getCenter();
+			const {lat: currentLat, lng: currentLon} = currentCenter;
+
+			if (lat !== currentLat || lon !== currentLon || zoom !== currentZoom) {
+				const zoomToSet = currentZoom !== zoom ? zoom : currentZoom;
+				map.setView({lat, lng: lon}, zoomToSet, {
+					zoom: {animate: false},
+					pan: {animate: false},
+				});
+			}
+		}
+	}, [map, zoom, lat, lon]);
+
+	const onZoomEnd = useCallback(() => {
+		const currentZoom = map.getZoom();
+		if (onZoomChange && currentZoom !== zoom) {
+			const currentCenter = map.getCenter();
+			onZoomChange(currentZoom);
+			onCenterChange([currentCenter.lat, currentCenter.lng]);
+		}
+	}, [map, zoom]);
+
+	const onMoveEnd = useCallback(() => {
+		const currentCenter = map.getCenter();
+		let {lat: currentLat, lng: currentLon} = currentCenter;
+		// call onViewChange if current map center is different from center props
+		if (onCenterChange && (currentLat !== lat || currentLon !== lon)) {
+			const currentZoom = map.getZoom();
+			onCenterChange([currentLat, currentLon], currentZoom);
+		}
+	}, [onCenterChange, map, lat, lon]);
+
+	useEffect(() => {
+		map?.on('moveend', onMoveEnd);
+		map?.on('zoomend', onZoomEnd);
+		return () => {
+			map?.off('moveend', onMoveEnd);
+			map?.off('zoomend', onZoomEnd);
+		};
+	}, [map, onMoveEnd, onZoomEnd]);
+
+	const displayMap = useMemo(
+		() => (
+			<MapContainer
+				ref={setMap}
+				center={center}
+				zoom={zoom}
+				className="m2m-Map"
 				crs={crs}
-			/>
-		</MapContainer>
+				zoomControl={zoomControl}
+				attributionControl={attributionControl}
+			>
+				{children}
+			</MapContainer>
+		),
+		[],
 	);
+
+	return <>{displayMap}</>;
 }
 
 Map.propTypes = {
 	crs: PropTypes.object,
 	center: PropTypes.array,
 	zoom: PropTypes.number,
+	zoomControl: PropTypes.bool,
+	onCenterChange: PropTypes.func,
+	onZoomChange: PropTypes.func,
+	attributionControl: PropTypes.bool,
+	children: PropTypes.node,
 };
 
 export default Map;
--- src/components/MapSet/index.jsx
+++ src/components/MapSet/index.jsx
@@ -2,9 +2,51 @@ import crs from '../../utils/crs.js';
 import Map from '../Map';
 
 import './style.css';
+import {useState} from 'react';
+import {WMSTileLayer} from 'react-leaflet';
 
 function MapSet() {
-	return <Map center={[49.73, 13.75]} zoom={14} crs={crs.get('EPSG:5514')} />;
+	const [center, setCenter] = useState([49.735, 13.759]);
+	const [zoom, setZoom] = useState(16);
+
+	return (
+		<div className="m2m-MapSet">
+			<Map
+				center={center}
+				zoom={zoom}
+				onCenterChange={setCenter}
+				onZoomChange={setZoom}
+				crs={crs.get('EPSG:5514')}
+				attributionControl={false}
+			>
+				<WMSTileLayer
+					url={'https://gis.cenia.cz/mapcache/ortofotomapa_historicka/wms?'}
+					params={{
+						layers: 'orto',
+					}}
+					crs={crs.get('EPSG:5514')}
+				/>
+			</Map>
+			<Map
+				center={center}
+				zoom={zoom}
+				onCenterChange={setCenter}
+				onZoomChange={setZoom}
+				crs={crs.get('EPSG:5514')}
+				zoomControl={false}
+			>
+				<WMSTileLayer
+					url={
+						'https://ags.cuzk.cz/arcgis1/services/ORTOFOTO/MapServer/WMSServer'
+					}
+					params={{
+						layers: '0',
+					}}
+					crs={crs.get('EPSG:5514')}
+				/>
+			</Map>
+		</div>
+	);
 }
 
 export default MapSet;
