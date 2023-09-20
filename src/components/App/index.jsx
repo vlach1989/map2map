f3b45f17124b2458911898bdeae3aee50146f7d0
PavelVlach <vlach1989@gmail.com>
2023-09-20 18:55:58 +0200

    Decouple top bar

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,27 +1,14 @@
 import {MantineProvider} from '@mantine/core';
 import MapSet from '../MapSet/index.jsx';
+import TopBar from '../TopBar/index.jsx';
 import './style.css';
-import SearchPlace from '../SearchPlace/index.jsx';
-import {useState} from 'react';
 
 function App() {
-	const [zoom, setZoom] = useState();
-	const [lat, setLat] = useState();
-	const [lon, setLon] = useState();
-
-	const onLocationChange = location => {
-		setZoom(location.zoom);
-		setLat(location.lat);
-		setLon(location.lon);
-	};
-
 	return (
 		<MantineProvider withGlobalStyles withNormalizeCSS>
 			<div className="m2m-App">
-				<div className="m2m-TopBar">
-					<SearchPlace onLocationChange={onLocationChange} />
-				</div>
-				<MapSet lat={lat} lon={lon} zoom={zoom} />
+				<TopBar />
+				<MapSet />
 			</div>
 		</MantineProvider>
 	);
---
+++ src/components/TopBar/index.jsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+function TopBar() {
+	return <div className="m2m-TopBar"></div>;
+}
+
+export default TopBar;
