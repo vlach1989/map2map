7395e209142f090fa06a920ef7f8216886e17700
PavelVlach <vlach1989@gmail.com>
2023-10-05 23:20:22 +0200

    maps: set to url

--- src/state/maps/actions.js
+++ src/state/maps/actions.js
@@ -1,4 +1,20 @@
 import ActionTypes from '../../constatnts/ActionTypes.js';
+import url from '../../utils/url.js';
+import Select from '../Select.js';
+
+function setMapLayer(mapKey, layerKey) {
+	return (dispatch, getState) => {
+		dispatch(actionSetMapLayer(mapKey, layerKey));
+
+		// update url
+		const maps = Select.maps.getAll(getState());
+		const mapKeys = maps?.map(map => map.key);
+		url.set('maps', mapKeys);
+		maps.forEach(map => {
+			url.set(map.key, map.layer);
+		});
+	};
+}
 
 const actionSetMapLayer = (mapKey, layerKey) => {
 	return {
@@ -9,5 +25,5 @@ const actionSetMapLayer = (mapKey, layerKey) => {
 };
 
 export default {
-	setMapLayer: actionSetMapLayer,
+	setMapLayer,
 };
