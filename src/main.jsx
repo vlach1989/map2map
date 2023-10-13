e5889adfb9a9d51f0000c9bc5996af6974b6cb42
PavelVlach <vlach1989@gmail.com>
2023-10-13 09:15:11 +0200

    setup css variables

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import store from './state/Store';
 
 import App from './components/App';
-import './index.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
