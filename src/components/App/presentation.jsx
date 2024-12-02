e5c253046bbcdd529a1614b84f1613834fd683b1
PavelVlach <vlach1989@gmail.com>
2024-12-02 11:22:21 +0100

    Remove unneeded components

--- src/components/App/presentation.jsx
+++ src/components/App/presentation.jsx
@@ -1,14 +1,12 @@
 import '@mantine/core/styles.css';
 import {MantineProvider} from '@mantine/core';
 import MapSet from '../MapSet';
-import TopBar from '../TopBar';
 import './style.css';
 
 const App = () => {
 	return (
 		<MantineProvider>
 			<main className="m2m-App">
-				<TopBar />
 				<MapSet />
 			</main>
 		</MantineProvider>
