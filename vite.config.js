3f1b6b43f7dc3694b898d3b68159e14002f05b29
PavelVlach <vlach1989@gmail.com>
2025-08-14 21:49:56 +0200

    fix path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/map2map',
 	plugins: [
 		react({
 			// Enables React Fast Refresh + JSX transform
